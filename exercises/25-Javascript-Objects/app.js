dc629a74e57778053096a3613c8703242ec02b8e
Tibisfly <vielmatorotibisay@gmail.com>
2021-03-27 16:08:33 +0000

    25 finally ended

--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -30,9 +30,51 @@ function addAllFamilyLuckyNumbers(anArray){
   
   return sumOfAllLuckyNumbers;
 }
+//Instruction 1
+function changeLuckyNumberJhonDoeInPostion(position, newNumber){
+    person.lucky_numbers[position] = newNumber  
+    return (person);
+}
+
+//Instruction 2
+function addNewMemberToFamily(name, age, gender, luckyNumbers, significantOther){
+    var newMember = {
+            name: name,
+            lastname: family.lastname,
+            age: age,
+            gender: gender,
+            lucky_numbers: luckyNumbers,
+            significant_other: significantOther
+        };
+
+    family.members.push(newMember);
+  
+  return (family);
+}
+
+//Instruction 3
+function SumOfFamilyLuckyNumbers(){
+  var sum = 0;
+    for (var i = 0; i < family.members.length; i++) {
+        for (var j = 0; j < family.members[i].lucky_numbers.length; j++) {
+                sum = sum + family.members[i].lucky_numbers[j]
+        }  
+    }
+  return (sum);
+}
+
 
 //Enter all your code here:
 
 
 //Do not make changes below:
-console.log(addAllFamilyLuckyNumbers(family.members)); 
+// console.log(addAllFamilyLuckyNumbers(family.members)); 
+
+
+// console.log("Instruction 1"); 
+changeLuckyNumberJhonDoeInPostion(3, 33)
+
+// console.log("Instruction 2"); 
+addNewMemberToFamily("Jimmy", "13", "male", [1, 2, 3, 4], null) 
+// console.log("Instruction 3"); 
+console.log(SumOfFamilyLuckyNumbers()); 
